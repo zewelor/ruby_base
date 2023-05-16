09a53c9fd2cf9bf1b6af877793396a1a07664d11
zewelor <zewelor@gmail.com>
2023-05-16 17:19:30 +0200

    Provide helper script

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Ruby base projects
 
-## Ruby cli
+## Ruby Base Clone and Rename
+This repository provides a bash script, clone_and_rename.sh, which clones a specified Ruby base repository and then renames certain predefined elements.
 
-Checkout branch `ruby-cli` for a simple ruby cli project.
+The script performs the following operations:
+
+1. Clones the ruby_base repository from Github into a directory with a user-provided name.
+2. Deletes the .git directory inside the newly created directory and re-initializes Git.
+3. Renames any files or directories within the new directory that start with "base_projects" to start with the user-provided name.
+4. Replaces any occurrences of "BaseProject" in the files within the new directory with the CamelCase version of the user-provided name.
+5. Replaces any occurrences of "base_projects" in the files within the new directory with the user-provided name.
+
+### Usage
+To use the script, you need to download it and make it executable. Here's a one-liner that accomplishes this, runs the script, and then deletes it:
+
+```bash
+wget https://raw.githubusercontent.com/zewelor/ruby_base/main/clone_and_rename.sh && chmod +x clone_and_rename.sh && ./clone_and_rename.sh new_project && rm clone_and_rename.sh
+```
+In the command above, replace new_project with your desired name. This command will clone the repository and replace all occurrences of "base_projects" and "BaseProject" with your provided name and its CamelCase equivalent, respectively.
+
+### Help
+If you need help or want to view the usage instructions, you can download the script and run:
+
+```bash
+./clone_and_rename.sh -h
+```
---
+++ clone_and_rename.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Function to display help
+function display_help {
+  echo "Usage: $0 [OPTION]... [NEW_NAME]"
+  echo "Clone a repository, then rename 'base_projects' to a new name in file/directory names and replace 'BaseProject' string inside files."
+  echo ""
+  echo "Mandatory arguments:"
+  echo "  new_name     new name to replace 'base_projects'"
+  echo ""
+  echo "Optional arguments:"
+  echo "  -h, --help   display this help and exit"
+}
+
+# Check number of arguments
+if [ "$#" -lt 1 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  display_help
+  exit 0
+fi
+
+# Configurable constants
+OLD_NAME="base_projects"
+
+# Arguments
+NEW_NAME="$1"
+
+# Convert new_name to CamelCase using Ruby
+NEW_NAME_CAMEL=$(ruby -e "puts '${NEW_NAME}'.split('_').collect(&:capitalize).join")
+
+# Convert base name to CamelCase using Ruby
+OLD_NAME_CAMEL=$(ruby -e "puts '${OLD_NAME}'.split('_').collect(&:capitalize).join")
+
+# Function to clone repository
+function clone_repository {
+  git clone --depth 1 https://github.com/zewelor/ruby_base.git -b ruby-cli "${NEW_NAME}"
+}
+
+# Clone repository
+clone_repository
+
+# Directory to process
+DIR_PATH="${NEW_NAME}"
+
+# Check if the directory exists
+if [ -d "$DIR_PATH" ]; then
+  # Delete .git directory and reinitialize
+  rm -rf "${DIR_PATH}/.git"
+  cd "${DIR_PATH}"
+  git init
+  cd ..
+
+  # Rename directories and files
+  find "$DIR_PATH" -name "${OLD_NAME}*" | while read FILE; do
+    NEW_FILE=$(echo $FILE | sed "s/${OLD_NAME}/${NEW_NAME}/g")
+    mv "$FILE" "$NEW_FILE"
+  done
+
+  # Rename content inside files
+  find "$DIR_PATH" -type f -exec sed -i "s/$OLD_NAME_CAMEL/$NEW_NAME_CAMEL/g" {} \;
+  find "$DIR_PATH" -type f -exec sed -i "s/$OLD_NAME/$NEW_NAME/g" {} \;
+else
+  echo "Directory does not exist"
+fi
+
