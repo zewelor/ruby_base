3546a0b12fb6d63cea5e518825deb98d99d22750
zewelor <zewelor@gmail.com>
2025-03-05 08:49:51 +0000

    feat: Add dockerized.sh for easy command execution in Docker
    This commit introduces a new script, `dockerized.sh`, which defines functions to easily execute commands within a Docker container. It checks if the container is running and uses `docker compose exec` if it is, otherwise uses `docker compose run`. Aliases are created for common commands like `rails`, `bundle`, `rake`, etc. It also sets the `LEFTHOOK_BIN` environment variable.

---
+++ dockerized.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+docker_compose_run_on_exec () {
+  local container_name="$1"
+  shift
+
+  if docker compose ps | grep -q $container_name
+  then
+    docker compose --progress quiet exec -it $container_name "$@"
+  else
+    docker compose --progress quiet run --rm -it $container_name "$@"
+  fi
+}
+
+# Declare functions for each name
+names=("rails" "bundle" "rake" "gem" "standardrb" "rubocop" "rspec" "lefthook" "spring" "brakeman")
+
+for name in "${names[@]}"
+do
+  unset -f $name 2> /dev/null
+  eval "
+  function $name() {
+    docker_compose_run_on_exec app $name \"\$@\"
+  }
+  "
+done
+
+export LEFTHOOK_BIN="bin/lefthook" # Set the lefthook bin path
+echo "Dockerized aliasses set"
